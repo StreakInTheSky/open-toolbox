f98ad1f99f49c67cdece5c0b848ea4185cd7f06b
StreakInTheSky <streakinthesky@gmail.com>
2017-02-03 20:42:56 -0800

    Setup sass

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    // jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    server = require('gulp-server-livereload');
+
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('src/css/style.scss')
+    .pipe(sass({ style: 'expanded', }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('public/css'))
+    // .pipe(rename({ suffix: '.min' }))
+    // .pipe(minifycss())
+    // .pipe(gulp.dest('public/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('public/js'))
+    // .pipe(rename({ suffix: '.min' }))
+    // .pipe(uglify())
+    // .pipe(gulp.dest('html/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('public/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+// Clean
+gulp.task('clean', function() {
+  return gulp.src(['public/css', 'public/js', 'public/image'], {read: false})
+    .pipe(clean());
+});
+
+// Server
+gulp.task('webserver', function() {
+  gulp.src('./public/')
+    .pipe(server({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+	gulp.run('watch')
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.run(['styles', 'scripts', 'images']);
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    gulp.run('styles');
+  });
+
+  // Watch .js files
+  gulp.watch('src/scripts/resources/*.js', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    gulp.run('scripts');
+  });
+
+  // Watch image files
+  gulp.watch('src/images/**/*', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    gulp.run('images');
+  });
+
+});
---
+++ src/js/app.js
@@ -0,0 +1,81 @@
+var MOCK_DATA = {
+	listings: [
+		{
+			id: 'aaaaaaa',
+			userId: 111111,
+			category: 'home tools',
+			toolName: 'screwdriver',
+			description: 'brand new',
+			rate: 100,
+			availability: [
+				{
+					start: '1486006623722',
+					end: '1486006683723'
+				}
+			],
+			images: ['../images/screwdriver1.jpg']
+		},
+		{
+			id: 'bbbbbbb',
+			userId: 111111,
+			category: 'home tools',
+			toolName: 'screwdriver',
+			description: 'brand new',
+			rate: 50,
+			availability: [
+				{
+					start: '1486006623722',
+					end: '1486006683723'
+				}
+			],
+			images: ['../images/screwdriver1.jpg']
+		},
+		{
+			id: 'ccccccc',
+			userId: 111111,
+			category: 'home tools',
+			toolName: 'screwdriver',
+			description: 'brand new',
+			rate: 25,
+			availability: [
+				{
+					start: '1486006623722',
+					end: '1486006683723'
+				}
+			],
+			images: ['../images/screwdriver1.jpg']
+		}
+	]
+}
+
+function getListings(callbackFn) {
+    setTimeout(function(){ callbackFn(MOCK_DATA)}, 100);
+}
+
+// this function stays the same when we connect
+// to real API later
+function displayListings(data) {
+    for (index in data.listings) {
+       $('ol.results').append(
+       	'<div class="result-listing">' +
+       		'<div class="listing-row-one">' +
+       			'<h3 class="tool-name">' + data.listings[index].toolName + '</h3>' +
+       			'<p class="rate">$' + ((data.listings[index].rate)/Math.pow(10, 2)).toFixed(2) + '/day</p>' +
+       		'</div>' +
+       		'<div class="listing-row-two">' + 
+       			'<img src="' + data.listings[index].images[0] + '"></img>' +
+        		'<p class="description">' + data.listings[index].description + '</p>'+
+        	'</div>' +
+        '</div>');
+    }
+}
+
+// this function can stay the same even when we
+// are connecting to real API
+function getAndDisplayListings() {
+    getListings(displayListings);
+}
+
+$(function() {
+    getAndDisplayListings();
+})
