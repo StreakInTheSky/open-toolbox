bcd28e24681c84b0d05dc0e012af2087f393fc1a
StreakInTheSky <streakinthesky@gmail.com>
2017-03-19 00:48:23 -0700

    configure to setup testing of API

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
+const {Tool} = require('../model');
+const {PORT, TEST_DATABASE_URL} = require('../config');
 
 const should = chai.should();
 
