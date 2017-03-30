7e6b7ee141c0e749b9add0eeb088d31a2925cc55
StreakInTheSky <streakinthesky@gmail.com>
2017-03-29 20:10:11 -0700

    Update url for db

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/open-toolbox';
+                      // 'mongodb://localhost/open-toolbox';
+                      'mongodb://ross:screwdriver@ds141950.mlab.com:41950/open-toolbox';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                        global.TEST_DATABASE_URL ||
                       'mongodb://localhost/test-open-toolbox';
